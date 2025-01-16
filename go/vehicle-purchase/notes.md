668c97282606a63ada6899dad0962b0342943b66
alixanderthegreat <me@alixandercourt.com>
2025-01-16 15:31:20 -0700

    feat: implement ChooseVehicle

--- go/vehicle-purchase/notes.md
+++ go/vehicle-purchase/notes.md
@@ -3,3 +3,10 @@ I know that I am actually getting better:
 return (kind == "car" || kind == "truck")
 ```
 I would have never made something like this had I not taken these exercises. 
+
+And another benefit to this exercises that I can see is that I have learned things about go that I wouldn't have otherwise known:
+```go
+a_comes_before_b := "apple" < "banana" // true
+b_comes_before_a := "apple" > "banana" // false
+```
+I didn't know that you could lexiconographically compare strings... 🤯
--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -7,7 +7,11 @@ func NeedsLicense(kind string) (need bool) {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	msg := " is clearly the better choice."
+	if option1 < option2 {
+		return option1 + msg
+	}
+	return option2 + msg
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
