f802bf5f5351b0538a1caebb101b707796d0d173
alixanderthegreat <me@alixandercourt.com>
2025-01-20 18:06:20 -0700

    feat: implement Drive

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -33,7 +33,15 @@ func NewTrack(distance int) Track {
 // Drive drives the car one time. If there is not enough battery to drive one more time,
 // the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.battery < car.batteryDrain {
+		return car
+	}
+	return Car{
+		speed:        car.speed,
+		battery:      car.battery - car.batteryDrain,
+		distance:     car.distance + car.speed,
+		batteryDrain: car.batteryDrain,
+	}
 }
 
 // CanFinish checks if a car is able to finish a certain track.
