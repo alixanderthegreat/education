549e44fa1655bfaa5fc7acadd9103163661afd8f
alixanderthegreat <me@alixandercourt.com>
2025-01-26 09:34:33 -0700

    fix: state var more clearly

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -1,6 +1,9 @@
 package chance
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
@@ -9,13 +12,22 @@ func RollADie() int {
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
+	// define iterator at a higher scope
+	var i int
 	for {
-		// https://pkg.go.dev/math/rand#NormFloat64
-		var desiredStdDev, desiredMean, r float64 = 3.6, 4.2, rand.Float64()
-		r = r * desiredStdDev * desiredMean
-		if r > 0 && r < 12.0 {
-			return r
+		i++ // iterate over the iterator
+		// produce a deterministically weighted random float
+		var ( // https://pkg.go.dev/math/rand#NormFloat64
+			random_float              = rand.NormFloat64()
+			desired_standard_devation = 3.6
+			desired_mean              = 4.2
+			energy                    = random_float * desired_standard_devation * desired_mean
+		)
+		if energy > 0 && energy < 12.0 {
+			fmt.Println("iteration: ", i, " energy:", energy)
+			return energy
 		}
+
 	}
 }
 
