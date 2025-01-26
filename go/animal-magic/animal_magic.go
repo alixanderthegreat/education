6230f1c981bb5476ebffd3b5d67dcfc0ddaeffd8
alixanderthegreat <me@alixandercourt.com>
2025-01-26 09:52:06 -0700

    fix: integrate community feedback into GenerateWandEnergy

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -11,23 +11,7 @@ func RollADie() int {
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	// define iterator at a higher scope
-	var i int
-	for {
-		i++ // iterate over the iterator
-		// produce a deterministically weighted random float
-		var ( // https://pkg.go.dev/math/rand#NormFloat64
-			random_float              = rand.NormFloat64()
-			desired_standard_devation = 3.6
-			desired_mean              = 4.2
-			energy                    = random_float * desired_standard_devation * desired_mean
-		)
-		if energy > 0 && energy < 12.0 {
-			// fmt.Println("iteration: ", i, " energy:", energy)
-			return energy
-		}
-
-	}
+	return rand.Float64() * 12
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
