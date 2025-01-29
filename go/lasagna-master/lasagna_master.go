9409066b236852596429acec4d3abc2a1b00a6b5
alixanderthegreat <me@alixandercourt.com>
2025-01-29 08:44:00 -0700

    fix: clean stale commentary

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -20,12 +20,3 @@ func AddSecretIngredient(preferences, prearainged []string) {
 func ScaleRecipe(quantities []float64, portions int) []float64 {
 	return nil
 }
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
