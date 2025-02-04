d5c2d9bd21423b7ef05ff0640e475fac5512de66
alixanderthegreat <me@alixandercourt.com>
2025-02-04 06:07:05 -0700

    feat: implement InterestRate function

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -2,6 +2,16 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return float32(3.213)
+	case balance >= 0 && balance < 1000:
+		return float32(0.5)
+	case balance >= 1000 && balance < 5000:
+		return float32(1.621)
+	case balance >= 5000:
+		return float32(2.475)
+	}
 	return float32(0)
 }
 
