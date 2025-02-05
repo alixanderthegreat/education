8e58776e53a27d20f835e7dcc30b60cfcbea920b
alixanderthegreat <me@alixandercourt.com>
2025-02-05 07:11:45 -0700

    feat: implement err_checker function & refactor

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,33 +1,34 @@
 package booking
 
 import (
+	"fmt"
 	"time"
 )
+
+func err_checker(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
 	t, err := time.Parse("1/02/2006 15:04:05", date)
-	if err != nil {
-		panic(err)
-	}
+	err_checker(err)
 	return t
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
 	t, err := time.Parse("January 2, 2006 15:04:05", date)
-	if err != nil {
-		panic(err)
-	}
+	err_checker(err)
 	return t.Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
 	t, err := time.Parse("Monday, January 2, 2006 15:04:05", date)
-	if err != nil {
-		panic(err)
-	}
+	err_checker(err)
 	h, _, _ := t.Clock()
 
 	return h >= 12 && h <= 18
@@ -36,9 +37,7 @@ func IsAfternoonAppointment(date string) bool {
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
 	t, err := time.Parse("1/2/2006 15:04:00", date)
-	if err != nil {
-		panic(err)
-	}
+	err_checker(err)
 
 	return `You have an appointment on ` + t.Format("Monday, January 2, 2006, at 15:04.")
 }
