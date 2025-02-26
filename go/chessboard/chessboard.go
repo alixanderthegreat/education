903db2cfb0afb69ab2e1c2455118e4c07899a558
alixanderthegreat <me@alixandercourt.com>
2025-02-25 17:39:31 -0700

    feat: include File type

--- go/chessboard/chessboard.go
+++ go/chessboard/chessboard.go
@@ -1,6 +1,7 @@
 package chessboard
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
+type File []bool
 
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
 
