1d9784a2618463923933290a3148cbf44ab8655e
alixanderthegreat <me@alixandercourt.com>
2025-03-09 19:00:02 -0600

    fix: remove stale commentary

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -4,7 +4,6 @@ import (
 	"strconv"
 )
 
-// TODO: define the 'Drive()' method
 func (c *Car) Drive() {
 	if c.battery-c.batteryDrain > 0 {
 		c.battery -= c.batteryDrain
@@ -12,18 +11,15 @@ func (c *Car) Drive() {
 	}
 }
 
-// TODO: define the 'DisplayDistance() string' method
 func (c Car) DisplayDistance() string {
 	return "Driven " + strconv.Itoa(c.distance) + " meters"
 }
 
-// TODO: define the 'DisplayBattery() string' method
 func (c Car) DisplayBattery() string {
 	return "Battery at " + strconv.Itoa(c.battery) + "%"
 
 }
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
 func (c Car) CanFinish(trackDistance int) bool {
 	return trackDistance <= (c.battery/c.batteryDrain)*(c.speed)
 }
