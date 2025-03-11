92d018e36a6923b2423a8a0ad11844dece248a7a
alixanderthegreat <me@alixandercourt.com>
2025-03-10 19:01:15 -0600

    minor: complete exercise ("not my favorite lesson")

---
+++ go/parsing-log-files/notes.md
@@ -0,0 +1,4 @@
+I unfortunately am terrible at regexp. I am not going to lie in saying that I had to use the bot, and even then... I am not to confident in the solutions; in some ways I feel like I cheated my self... by I would have just handled those exercises differently than use regexp. I know... kind of puts me in this weird catch22:{
+    "on the one hand, if I do it my way... did I learn regexp like the lesson suggests?",
+    "on the other hand, if I use the bot... did I learn regexp like the lesson suggests?" 
+}; in either case, I didn't learn regexp today, I only gathered a little more insight into a world of pattern recognition that I am not accustomed too nor am I looking to be at the moment. 
--- go/parsing-log-files/parsing_log_files.go
+++ go/parsing-log-files/parsing_log_files.go
@@ -1,21 +1,42 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	ok, err := regexp.MatchString(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`, text)
+	if err != nil {
+		return false
+	}
+	return ok
 }
-
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	return regexp.MustCompile(`<>|<\*>|<~~~>|<=>|<-\*~\*->`).Split(text, -1)
 }
-
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	r := regexp.MustCompile(`(?i)".*password.*"`)
+	var count int
+	for i := range lines {
+		matches := r.FindAllString(lines[i], -1)
+		count += len(matches)
+	}
+	return count
 }
-
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	return regexp.MustCompile(`end-of-line\d+`).ReplaceAllString(text, "")
 }
-
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	var result []string
+	for _, log := range lines {
+		match := regexp.MustCompile(`\bUser\s+([A-Za-z0-9_]+)\b`).FindStringSubmatch(log)
+		if match != nil {
+			username := match[1]
+			result = append(result, fmt.Sprintf("[USR] %s %s", username, log))
+		} else {
+			result = append(result, log)
+		}
+	}
+	return result
 }
