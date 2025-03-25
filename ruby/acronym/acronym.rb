99f055e36eb9e61f9ec015e2d836cc4bf348bbcb
alixanderthegreat <me@alixandercourt.com>
2025-03-25 14:11:13 -0600

    feat: implement Acronym module

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,37 @@
-=begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
+module Acronym
+  class << self
+    def abbreviate(input)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
-=end
+      # Let's have this result contain an empty string
+      result = ""
+
+      # Let's select the first letter of the array
+      first_letter = 0
+
+      # Iterate through the input
+      input.strip().split().each() do |part|
+
+        # Handle the case when a hyphen `-`` is used 
+        if part.include?("-")
+
+          # Strip and split the part and split it into sub strings
+          part.strip().split("-") do |sub|
+
+            # Concatenate the first letter of the each sub string to the result
+            result += sub[first_letter]
+          
+          end
+        else # Concatenate the first letter of each part string to the result
+          result += part[first_letter]
+        end
+      end
+      
+      # Modify result to uppercase
+      result.upcase!()
+
+      # Return result
+      return result
+    end
+  alias_method :call, :abbreviate
+  end
+end
