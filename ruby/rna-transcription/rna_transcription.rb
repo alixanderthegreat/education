76b6a974dab045f6ed5374d10895553c5a3203a3
alixanderthegreat <me@alixandercourt.com>
2025-03-28 17:40:36 -0600

    feat: implement Complement module

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,7 +1,17 @@
-=begin
-Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
-`rna_transcription_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/rna-transcription` directory.
-=end
+module Complement
+  class << self
+    def of_dna(input)
+      return input if input.nil? || input.empty?
+      result = input.chars().map() do | letter | 
+        case letter
+          when 'C' then 'G'
+          when 'G' then 'C'
+          when 'T' then 'A'
+          when 'A' then 'U'
+          else letter
+        end
+      end
+      result.join()
+    end
+  end
+end
