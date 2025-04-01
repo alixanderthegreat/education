3d5e2329e36444411cba118c84dde6ed20af4839
alixanderthegreat <me@alixandercourt.com>
2025-04-01 15:14:59 -0600

    feat: implement IsbnVerifier module

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,7 +1,13 @@
-=begin
-Write your code for the 'ISBN Verifier' exercise in this file. Make the tests in
-`isbn_verifier_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/isbn-verifier` directory.
-=end
+module IsbnVerifier
+    def self.valid?(input)
+        return false if input.nil? || input.empty? 
+        isbn = input.delete("-")
+        return false if isbn.length != 10
+        return false unless isbn[0..8].chars().all?() { |char|  char <= "9" && char >= "0" }
+        return false unless ( ( isbn[9] <= "9" && isbn[9] >= "0" ) || isbn[9].upcase() == "X" )
+        sum = isbn.chars().each_with_index().sum() do | digit, index |
+            ( digit.upcase() == "X" && index == 9 ) ? 10 : digit.to_i * ( isbn.length() - index )
+        end
+        sum % 11 == 0 
+    end
+end
