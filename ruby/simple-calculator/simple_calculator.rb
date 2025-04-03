303f782464880f5ed8124e3654b03499fd59fa30
alixanderthegreat <me@alixandercourt.com>
2025-04-02 18:27:43 -0600

    fix: raise exceptions prior to rescue loop

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -2,14 +2,14 @@ class SimpleCalculator
  
   UnsupportedOperation = Class.new(ArgumentError)
 
-  ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
+  ALLOWED_OPERATIONS = ['+', '/', '*'].freeze()
 
   def self.calculate(first_operand, second_operand, operation)
+    raise ArgumentError, "Operand must be integer" unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer) 
+    raise UnsupportedOperation, "Operation not allowed." unless ALLOWED_OPERATIONS.include?(operation)
     begin
-      raise ArgumentError, "Operand must be integer" unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer) 
-      raise UnsupportedOperation, "Operation not allowed." unless ALLOWED_OPERATIONS.include?(operation)
-      result = first_operand.send(operation, second_operand) 
-      return "%s %s %s = %s" % [ first_operand, operation, second_operand, result ]
+      result = first_operand.public_send(operation, second_operand) 
+      return "#{first_operand} #{operation} #{second_operand} = #{result}"
     rescue ZeroDivisionError
       return "Division by zero is not allowed."
     end
