5d4bde39daec2748445306bd330223e3a88f0a1e
alixanderthegreat <me@alixandercourt.com>
2025-04-04 17:17:45 -0600

    feat: implemetn AssemblyLine#production_rate_per_hour

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -4,7 +4,15 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    base = 221 * @speed
+    return base unless @speed > 4
+
+    modifier = 0.0
+    modifier = 0.77 if @speed == 10 
+    modifier = 0.80 if @speed == 9 
+    modifier = 0.90 if @speed > 4 && @speed < 9
+    result = (base * modifier)
+    return result
   end
 
   def working_items_per_minute
