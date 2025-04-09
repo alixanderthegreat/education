b7dbd8a179230c05c81e1ae8cb1968d065dba514
alixanderthegreat <me@alixandercourt.com>
2025-04-08 20:13:27 -0600

    fix: organize conditionals

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -3,14 +3,14 @@ class AssemblyLine
     @speed = speed
   end
 
-  def production_rate_per_hour
+  def production_rate_per_hour()
     base = 221 * @speed
     return base unless @speed > 4
 
     modifier = 0.0
     modifier = 0.77 if @speed == 10 
     modifier = 0.80 if @speed == 9 
-    modifier = 0.90 if @speed > 4 && @speed < 9
+    modifier = 0.90 if @speed < 9 && @speed > 4 
     result = (base * modifier)
     return result
   end
