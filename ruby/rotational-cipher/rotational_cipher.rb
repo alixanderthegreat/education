a04f7b0688c3d9a65c869d60c5af82ce7fff255b
alixanderthegreat <me@alixandercourt.com>
2025-04-09 17:23:13 -0600

    feat: implement RotationalCipher module

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,7 +1,26 @@
-=begin
-Write your code for the 'Rotational Cipher' exercise in this file. Make the tests in
-`rotational_cipher_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/rotational-cipher` directory.
-=end
+module RotationalCipher
+    class << self
+        LOWERCIPHER = ('a'..'z').to_a().join()
+        UPPERCIPHER = LOWERCIPHER.upcase()
+        def rotate(input, key)
+            message = []
+            return input if key.zero?() || key.nil?()
+            begin
+                for char in input.each_char()
+                    case char
+                    when "A".."Z"
+                        message.append(UPPERCIPHER[(UPPERCIPHER.index(char) + key) % 26])
+                    when "a".."z"
+                        message.append(LOWERCIPHER[(LOWERCIPHER.index(char) + key) % 26])
+                    else 
+                        message.append(char)
+                    end
+                end
+            rescue => e 
+                puts e.message
+            end
+            result = message.join()
+            return result
+        end
+    end
+end
