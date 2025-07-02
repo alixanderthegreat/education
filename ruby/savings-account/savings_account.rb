dc8adc7f8225177ed4dc1e366d58cac799930761
alixanderthegreat <me@alixandercourt.com>
2025-07-02 17:04:19 -0600

    feat: implement interest_rate method

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,6 +1,21 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    rates_of_return = [
+      0.5, 1.621, 2.475
+    ]
+    rates_of_borrowing =[
+      3.213
+    ]
+    case balance
+    when (0..999.999)
+      return rates_of_return[0]
+    when (1000..4999.999)
+      return rates_of_return[1]
+    when (5000..)
+      return rates_of_return[2]
+    when (..0)
+      return rates_of_borrowing[0]
+    end
   end
 
   def self.annual_balance_update(balance)
